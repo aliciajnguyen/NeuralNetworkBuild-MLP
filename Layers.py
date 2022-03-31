27053156e6bf9be74f30047c3a940b4456771de1
anguyen <alicia.j.nguyen@gmail.com>
2022-03-30 22:21:14 -0400

    about to radical refactor backprop

--- Layers.py
+++ Layers.py
@@ -2,23 +2,23 @@
 #define a base/parent class that the 3 types of layers will inherit from
 #3 different layer articulations after, 1 "edge" layer articulation
 
+import itertools
 import numpy as np
+import ActivationFunctions as af
 
-#GENERAL CASE
+#GENERAL STRUCTURE
+#Parent class, use for documenting other layers - necessary?
 class Layer:
-    
     # Will be overriden to have different constructor for each layer type
-    def __init__(self):
-        #self.activations each layer must have       
-        pass
-    
-    #FOR FORWARD PASS
+     #FOR FORWARD PASS
     def get_output(self, input):
         pass 
-
     #FOR BACKWARD PASS
     def get_input_grad(self, Y, output_grad):
         pass
+    #all classes inherit:
+    def get_activations():
+        return self.output
 
 
 # class for the input layer (called an edge because does special linear trasofrm with V)
@@ -35,39 +35,32 @@ class InputEdge(Layer):
         #OUTPUT OF LAYER
         #last saved as self.output in get_output
 
-        #WEIGHT PARAMETERS 
-        #will be V for the first layer
-        self.V = V
-
-        #BIAS
-        #will be the same for the whole layer
-        #dimensions according to the weight input passed (same number of cols)
-        #initialize to 1 (hyperparameter to tune?)
-        #learn later
-        #initialize in constructor
-        #self.b = b INCORPORATED IN TO WEIGHT VECTOR V
-        #self.b = np.ones(V.shape[0]) #WRONG
-        
-
-    #FOR FORWARD PASS
+        self.V = V         #WEIGHT PARAMETERS 
 
     #   input here is X (original data), dim = N x D
     #   function is must matrix multiplication (@) with V 
     #   output is z^l aka this layer's output
     def get_output(self, X):
-        #TODO should we be saving the input here too? prob not just output of last layer
-        #TODO is this matrix multiplication correct? check math
         output = (X @ self.V) 
         self.output = output #INPUT LAYER
         return output
 
-    #FOR BACKWARD PASS
-    #TODO
+    def get_params_iter(self):
+        """Return an iterator over the parameters."""
+        return itertools.chain(
+            np.nditer(self.W, op_flags=['readwrite']),
+            np.nditer(self.b, op_flags=['readwrite']))
+    
     def get_params_grad(self, X, output_grad):
-        pass
-
+        """Return a list of gradients over the parameters."""
+        JW = X.T @ output_grad
+        Jb = np.sum(output_grad, axis=0)
+        return [g for g in itertools.chain(
+            np.nditer(JW), np.nditer(Jb))]
+    
     def get_input_grad(self, Y, output_grad):
-        pass
+        """Return the gradient at the inputs of this layer."""
+        return output_grad @ self.V.T
 
 #hidden layer can have whatever relavent activation function
 
@@ -77,20 +70,17 @@ class InputEdge(Layer):
 #   and this will be the input to the next layer
 class HiddenLayer(Layer):
 
-    def __init__(self, activation_function):       
+    def __init__(self, activation_function_obj):       
         #INPUT TO LAYER
         # computed from last edge layer
         #input here is X aka z^l-1 (output of the last layer)
-        #calculated in first pass TODO
-
-        #ACTIVATION FUNCTION
-        self.activation_function = activation_function
-
-        #STORED LATER
+        #calculated in first pass
         #LAYER OUTPUT
         #self.output=  where the output of the activation function stored: activation_function (z)
 
-    #FOR FORWARD PASS
+        #ACTIVATION FUNCTION
+        self.activation_function = activation_function_obj.get_func()
+        self.activation_function_der = activation_function_obj.get_deriv()
 
     #input here is X aka z^l-1 (output of the last layer)
     #output is z^l aka this layer's output
@@ -98,25 +88,24 @@ class HiddenLayer(Layer):
         #compute activation function
         output = self.activation_function(z) #cross mult correct?
         
-        #need to append an extra col to output_hu for bias
-        #so dim will become M + 1 x 1
-        #must add bias to X input, so now D + 1 width - an extra col?
-        bias = np.ones((output.shape[0],1), dtype=float) #append space for each row 
+        #need to append an extra col to output for bias
+        #so dim will become ((M + 1) x N)
+        bias = np.ones((output.shape[0],1), dtype=float)
         output = np.append(output, bias, axis=1)
 
-        self.output = output #HID LAYER
-        print("Hidden layer output:")
+        self.output = output
+
+        print("Hidden layer output:") #debug
         print(output)
 
-        return output #this is what we want as the output to the next activation function
+        return output 
 
-    #FOR BACKWARD PASS
-    #TODO
-    def get_params_grad(self, X, output_grad):
-        pass
-
+    #get gradient of inputs at this layer
+    #pass the derivative of whatever activation function we're using
     def get_input_grad(self, Y, output_grad):
-        pass
+        
+
+        return np.multiply(self.activation_function_der(Y), output_grad)
 
 #the edge class performs the linear transformations (ie Wv + b)
 # note the input layer to first layer DOES NOT have an edge bc IS an edge
@@ -125,36 +114,21 @@ class HiddenLayer(Layer):
 #   and this will be the input to the next layer
 #   weight matrix should be W for the last layer
 class Edge(Layer):
-    def __init__(self, V):       
+    def __init__(self, V, isFinalEdge=False):       
         #INPUT TO LAYER
         #input here is X aka z^l-1 (output of the last layer)
-        #calculated in first pass TODO
-     
+        #OUTPUT
+        #self.output = where output of linear stord (WX + b)
+
         #PARAMETERS  (initialized in constructor of MLP)
         self.V = V
 
-        #LATER CALCULATED
-
-        #self.output = where output of linear stord (WX + b)
-        #dim M x 1
-
-        #BIAS
-        #defined in the Edge associated
-        #will be the same for the whole layer
-        #dimensions according to the weight input passed (same number of cols)
-        #initialize to 1 (hyperparameter to tune?)
-        #learn later
-        #self.b = b       INCORP INTO WEIGHT VECTOR
-
     # compute WX + b: z @ self.W + self.b
-    # b INCORP INTO WEIGHT VECTOR
     def get_output(self, z):
-        output = z @ self.V  # compute z @ self.W + self.b
+        output = z @ self.V  # compute z @ self.W + self.b, b INCORP INTO WEIGHT VECTOR
         self.output = output #EDGE
         return output
 
-    #FOR BACKWARD PASS
-    #TODO
     def get_params_grad(self, X, output_grad):
         pass
 
@@ -162,6 +136,25 @@ class Edge(Layer):
     def get_input_grad(self, Y, output_grad):
         pass
         #return output_grad @ self.W.Y
+        
+    def get_params_iter(self):
+        """Return an iterator over the parameters."""
+        return itertools.chain(
+            np.nditer(self.W, op_flags=['readwrite']),
+            np.nditer(self.b, op_flags=['readwrite']))
+
+    def get_params_grad(self, X, output_grad):
+        """Return a list of gradients over the parameters."""
+        JW = X.T @ output_grad
+        Jb = np.sum(output_grad, axis=0)
+        return [g for g in itertools.chain(
+            np.nditer(JW), np.nditer(Jb))]
+    
+    #gradients of inputs at this layer
+    def get_input_grad(self, Y, output_grad):
+        """Return the gradient at the inputs of this layer."""
+        #return output_grad @ self.V.Y
+        return output_grad @ self.V.T
 
 
 #layer task is to apply the relavent function for our task
@@ -170,39 +163,31 @@ class Edge(Layer):
 #though note to do so we'd have to change the cost function (and how it's computed)
 class OutputLayer(Layer):
 
-    def __init__(self, activation_function):       
+    def __init__(self, activation_function_obj, cost_function):       
+        self.activation_function = activation_function_obj.get_func()  #ACTIVATION FUNCTION
+        self.cost_function = cost_function              #COST FUNCTION
         #INPUT TO LAYER
         #input here is X aka z^l-1 (output of the last layer)
-        #calculated in first pass TODO
-
-        #ACTIVATION FUNCTION
-        self.activation_function = activation_function
-
+        #calculated in first pass
         #LAYER OUTPUT
-        #will be computed in get_output
-        #dim ? determined by the weight shape of the last yayer
-        #stored in self.output 
+        #will be computed in get_output, dim ? determined by the shape of weight matrix in last layer
+        #stored in self.output , here Yh
 
-    #FOR FORWARD PASS
-
-    #input here is X aka z^l-1 (output of the last layer)
-    #output is yh
+    #input here is X aka z^l-1 (output of the last layer), output is yh
     #here it will be softmax (multiclass), logistic (binary class) or identity (regression)
-    # each Y is dim C x D?
+    # each Yh is dim C x N?
     def get_output(self, z):
         output = self.activation_function(z) 
         self.output = output
         print("Final layer output:")
         print(output)
-        return output  #OUTPUT LAYER
+        return output 
 
-    #FOR BACKWARD PASS
-    #TODO
-
-    #NOTE that to expand this beyond softmax multiclsas we'd have to update cost
+    #cost function passed to constructor
     #categorical cross  entropy cost function
     def get_cost(self, Y, Yh):
-        return - np.multiply(Yh, np.log(Y)).sum() / Y.shape[0]
+        return self.cost_function(Y, Yh)
 
-    def get_input_grad(self, Y, output_grad):
-        return (Y - T) / Y.shape[0]
+    def get_input_grad(self, Y, Yh):
+        return (Y - Yh) / Y.shape[0]
+    #    return Yh - Y  #should be dy
